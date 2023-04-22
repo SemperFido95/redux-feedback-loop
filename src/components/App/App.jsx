23167f50260dfb9829dc7ef1b4e2fd3cbb7a22c6
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-04-21 20:27:03 -0400

    installed MUI and created Header and Welcome components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Header from '../Header';
+import Welcome from '../Welcome/Welcome';
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Header />
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+
+function Welcome() {
+
+    return (
+        <>
+            <h1>Welcome</h1>
+            <p>This is a feedback survery for how your day went today. This survey is 5 questions and should take less than 5 minutes to complete.</p>
+            <p>You may submit as many surveys as you would like! Click the button belwo to begin.</p>
+            <button>Begin Survey</button>
+        </>
+    )
+}
+
+export default Welcome;
