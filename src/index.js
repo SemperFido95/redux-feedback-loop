05f9b89cc9e9332c71bc400cf63fe7d3c349d3dc
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-04-22 13:01:35 -0400

    set up redux store in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+//Reducers
+
+
+
+// Store
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+
+        }
+    ), 
+    applyMiddleware(logger)
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
